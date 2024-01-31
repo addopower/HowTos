8aeedf163f1be127e7518454792691ecb4dd4ff8
addopower <75721640+addopower@users.noreply.github.com>
2024-01-31 13:21:12 +0100

    crea script root

--- script.sh
+++ script.sh
@@ -8,51 +8,99 @@ apt remove unattended-upgrades
 echo "" >> "/etc/mysql/my.cnf"
 echo "[mysqld]" >> "/etc/mysql/my.cnf"
 echo "binlog_expire_logs_seconds=1" >> "/etc/mysql/my.cnf"
+echo "max_connections=1000" >> "/etc/mysql/my.cnf"
 mysql --user="root" --execute="CREATE USER 'comma6a'@'localhost' IDENTIFIED BY 'comma6a';GRANT ALL PRIVILEGES ON *.* TO 'comma6a'@'localhost' WITH GRANT OPTION;"
 #mysql --user="comma6a" --password="comma6a" --execute="CREATE DATABASE SERVER;"
+echo "32181" > porta_server_ws.txt
+echo "{\"prima\":32181,\"ultima\":32190}" > porte_server_ws.json
 
-home_path=$HOME
-for i in {32181..32190}
+touch script_start.sh
+printf "#!/bin/bash
+prima=\$(jq '.prima' porte_server_ws.json)
+ultima=\$(jq '.ultima' porte_server_ws.json)
+
+
+for ((i = \$prima; i <= \$ultima; i++))
 do
-printf "[Unit]
-Description=SlotServerWS $i daemon
+   printf \"[Unit]
+Description=SlotServerWS \$i daemon
 
 [Service]
 Restart=always
 RestartSec=3
 User=root
 TasksMax=100000
-WorkingDirectory=%s
-ExecStart=%s/ServerWS -p %i
+WorkingDirectory=%%s
+ExecStart=%%s/ServerWS -p \%i
 
 [Install]
 WantedBy=multi-user.target
-" $HOME $HOME $i > /etc/systemd/system/ServerWS$i.service
+\" \$HOME \$HOME \$i > /etc/systemd/system/ServerWS\$i.service
 done
-
-printf "[Unit]
-Description=SlotServer daemon
-
-[Service]
-Restart=always
-RestartSec=3
-User=root
-TasksMax=100000
-WorkingDirectory=%s
-ExecStart=%s/ServerHTTP
-
-[Install]
-WantedBy=multi-user.target
-" $HOME $HOME > /etc/systemd/system/ServerHTTP.service
 
 systemctl daemon-reload 
+
+for ((i = \$prima; i <= \$ultima; i++))
+do
+systemctl enable ServerWS\$i.service 
+systemctl start ServerWS\$i.service
+done
+" > script_start.sh
+
+touch script_stop.sh
+
+printf "#!/bin/bash
+prima=\$(jq '.prima' porte_server_ws.json)
+ultima=\$(jq '.ultima' porte_server_ws.json)
+
+for ((i = \$prima; i <= \$ultima; i++))
+do
+systemctl stop ServerWS\$i.service
+systemctl disable ServerWS\$i.service
+rm /etc/systemd/system/ServerWS\$i.service
+done
+
+systemctl daemon-reload
+" > script_stop.sh
+
+touch script_restart.sh
+
+printf "#!/bin/bash
+
+prima=\$(jq '.prima' porte_server_ws.json)
+ultima=\$(jq '.ultima' porte_server_ws.json)
+
+for ((i = \$prima; i <= \$ultima; i++))
+do
+	systemctl restart ServerWS\$i.service
+done
+" > script_restart.sh
+
+touch cambia.sh
+
+printf "#!/bin/bash
+
+# Legge il contenuto attuale del file
+current_content=\$(cat /root/porta_server_ws.txt)
+prima=\$(jq '.prima' porte_server_ws.json)
+ultima=\$(jq '.ultima' porte_server_ws.json)
+
+# Se il contenuto attuale è maggiore di 32190 o vuoto, imposta il nuovo contenuto a 32181; altrimenti, incrementa di uno il contenuto attuale
+if [ -z \"\$current_content\" ] || [ \$current_content -ge \$ultima ]; then
+  new_content=\$prima
+else
+  new_content=\$((current_content + 1))
+fi
+
+# Scrive il nuovo contenuto nel file
+echo \$new_content > /root/porta_server_ws.txt
+cat /root/porta_server_ws.txt
+" > cambia.sh
+
+./script_start.sh
+
 systemctl enable ServerHTTP.service 
 systemctl start ServerHTTP.service
-for i in {32181..32190}
-do
-systemctl enable ServerWS$i.service 
-systemctl start ServerWS$i.service
-done
 systemctl enable redis-server.service
 systemctl start redis-server.service
 
